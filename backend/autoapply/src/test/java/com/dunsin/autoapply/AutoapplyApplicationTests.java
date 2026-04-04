d9694844d5ec021401e9d13fca479a665431cd9e
DunsinK <133840905+DunsinK@users.noreply.github.com>
2026-04-04 12:38:43 -0500

    initial stuff

---
+++ backend/autoapply/src/test/java/com/dunsin/autoapply/AutoapplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunsin.autoapply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoapplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
